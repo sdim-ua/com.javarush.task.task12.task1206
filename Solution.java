36be62e275878ac4f25c4a52c7b41b33cd981661
sdim-ua <ingersol@yandex.ua>
2019-06-17 23:46:11 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,20 @@
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    
+    
+
+    //Напишите тут ваши методы
+    public static void print (int x){
+        System.out.print(x);
+    }
+    
+    public static void print (String x){
+        System.out.print(x);
+    }
+}
